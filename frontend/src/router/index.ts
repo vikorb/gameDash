b555197567bcb52185a6aee3f64cfa3bddc66091
victoriaoruba <victoriaoruba06@gmail.com>
2026-02-04 09:41:54 +0100

    feat: rename file for test

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,7 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import MapsListView from '@/views/MapsListView.vue'
 import MapFormView from '@/views/MapFormView.vue'
-import HomeView from '@/views/HomeView.vue'
+import HomeView from '@/views/TestHomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
