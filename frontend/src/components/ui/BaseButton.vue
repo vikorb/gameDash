2a77fd2e889b8d19032e94162200c7cefb711121
victoriaoruba <victoriaoruba06@gmail.com>
2026-02-04 15:38:12 +0100

    fix: clean code

--- frontend/src/components/ui/BaseButton.vue
+++ frontend/src/components/ui/BaseButton.vue
@@ -10,7 +10,7 @@
 </template>
 
 <script setup lang="ts">
-import { RouterLink } from 'vue-router'; // <--- AJOUTER CET IMPORT
+import { RouterLink } from 'vue-router';
 
 interface Props {
   text?: string;
